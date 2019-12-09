d82f4e62188654dea84f6363ba0745e458ee7f11
Carlos <carlosrafael-mg@hotmail.com>
2019-12-09 10:29:32 +0100

    Revert "Created Color and Material Garement is almost finished, still not sure what to do about the image"
    This reverts commit 146b3fe9f9d315d1c19ed9c2f71333ee24da837f.

--- models/Color.py
+++
@@ -1,6 +0,0 @@
-from Odoo import models
-
-class Color(models.Model):
-    __name = 'litTrends.color'
-    
-    name = fields.Char(required=True)
--- models/Material.py
+++
@@ -1,6 +0,0 @@
-from Odoo import models
-
-class Material(models.Model):
-    __name = 'litTrends.material'
-    
-    name = fields.Char(required=True)
