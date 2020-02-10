f0fc24a435c3d044a8dc33bf165da02f3a1f6dd7
Carlos <carlosrafael-mg@hotmail.com>
2020-02-10 09:38:02 +0100

    hope it works

--- __manifest__.py
+++ __manifest__.py
@@ -23,7 +23,7 @@
 
     # always loaded
     'data': [
-        # 'security/ir.model.access.csv',
+        'security/ir.model.access.csv',
         'views/garmentFormView.xml',
     ],
     # only loaded in demonstration mode
